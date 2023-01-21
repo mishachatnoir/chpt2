831f3bcf585af1aa2ce0f703fc1ac6839bf58212
MishaChatNoir <misha@DESKTOP-NICPFBD.localdomain>
2023-01-22 01:36:22 +0300

    s3 config

--- main.tf
+++ main.tf
@@ -1,3 +1,24 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-up-and-running-state-misha"
+    key = "global/s3/terraform.state"
+    region = "us-east-2"
+
+    dynamodb_table = "terraform-up-and-running-locks-misha"
+    encrypt = true
+  }
+}
+
+output "s3_bucket_arn" {
+  value = aws_s3_bucket.terrafrom_state.arn
+  description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table"
+}
+
 provider "aws" {
   region = "us-east-2"
 }
@@ -25,5 +46,16 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
     apply_server_side_encryption_by_default {
       sse_algorithm     = "AES256"
     }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name = "terraform-up-and-running-locks-misha"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
